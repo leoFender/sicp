5c3eea88a7d92dac737d0e1bd9fea58b639f952d
leo_fender <mc@fides.local>
2013-02-19 11:43:31 +0200

    1-3 improve (2 checks)

--- 1-3.rkt
+++ 1-3.rkt
@@ -1,9 +1,9 @@
 #lang racket
 (define (sqr x) (* x x))
 (define (max-sqr a b c)
-   (if (or (> a b) (> a c))
+   (if (> a b)
        (+ (sqr a) (sqr (if (> b c) b c)))
-       (+ (sqr b) (sqr c))))
+       (+ (sqr b) (sqr (if (> a c) a c)))))
          
    
    
