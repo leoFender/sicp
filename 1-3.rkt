f68958d4706eb4724e51fb6da9d0a3d6899b6321
leo_fender <mc@fides.local>
2013-02-19 10:21:46 +0200

    1-3 fix (multiplication -> sum)

--- 1-3.rkt
+++ 1-3.rkt
@@ -2,8 +2,8 @@
 (define (sqr x) (* x x))
 (define (max-sqr a b c)
    (if (or (> a b) (> a c))
-       (* (sqr a) (sqr (if (> b c) b c)))
-       (* (sqr b) (sqr c))))
+       (+ (sqr a) (sqr (if (> b c) b c)))
+       (+ (sqr b) (sqr c))))
          
    
    
